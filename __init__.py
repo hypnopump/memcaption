0eda67a12560c78b3015266f6971d3c127bac715
Eric Alcaide <ericalcaide1@gmail.com>
2018-07-10 23:20:15 +0200

    psuhing experimental

--- __init__.py
+++ __init__.py
@@ -79,7 +79,7 @@ def new_user():
 
 if __name__ == '__main__':
 	# # Deploying
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     # Debugging
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
