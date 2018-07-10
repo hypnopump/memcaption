40a8c15f22840f4e2c32a071c15a87bff6e0d16c
Eric Alcaide <ericalcaide1@gmail.com>
2018-07-10 23:31:48 +0200

    psuhing experimental

--- __init__.py
+++ __init__.py
@@ -97,7 +97,7 @@ def testing():
 
 if __name__ == '__main__':
 	# # Deploying
-	# port = int(os.environ.get("PORT", 5000))
-	# app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 	# Debugging
-	app.run(debug=True, host='0.0.0.0')
+	# app.run(debug=True, host='0.0.0.0')
