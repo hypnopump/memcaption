78d88feca23a5c497edb7226e2a963d7b0437781
Eric Alcaide <ericalcaide1@gmail.com>
2018-07-12 18:18:13 +0200

    serving to production

--- create_db.py
+++ create_db.py
@@ -3,11 +3,11 @@ from utils.models import User, Comment, Img
 
 db.create_all()
 
-db.session.add(User("Eric Alcaide", " ericalcaide1@gmail.com", "18933DB28DBE16A2E6C99C4D7EA53D370C4D214B604C5301F27714A114F7C7E9"))
-db.session.add(User("George Orwell", "kytorch@gmail.com", "0A0D1178725D8C146133786ADCE09B3F2A6C6414E76AB31B26D28AF8A1E22F5C"))
+db.session.add(User("Eric Alcaide", " ericalcaide1@gmail.com", "18933db28dbe16a2e6c99c4d7ea53d370c4d214b604c5301f27714a114f7c7e9"))
+# db.session.add(User("George Orwell", "kytorch@gmail.com", "0a0d1178725d8c146133786adce09b3f2a6c6414e76ab31b26d28af8a1e22f5c"))
 
-db.session.add(Comment("eric_alcaide", "me in the mornin'", 1, 0, ""))
-db.session.add(Comment("George Orwell", "when you expect A+ but get C", 1, 0, ""))
+db.session.add(Comment("Eric Alcaide", "'Tomorrow is Monday'", 1, 0, ""))
+# db.session.add(Comment("George Orwell", "when you expect A+ but get C", 1, 0, ""))
 
 db.session.add(Img("first.png", "Pexels", "pexels.com"))
 db.session.add(Img("second.png", "Pexels", "pexels.com"))
